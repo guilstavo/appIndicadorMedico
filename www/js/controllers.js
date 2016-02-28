97e77cefa75f47ae1c7ae35b39c26d90b5beda28
Gustavo <gustavomartinis@gmail.com>
2016-02-28 03:09:12 -0300

    Pegando dados de especialidades do json.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,8 +1,16 @@
 angular.module('app.controllers', [])
   
-.controller('indicadorMDicoCtrl', function($scope, $ionicNavBarDelegate) {
+.controller('indicadorMDicoCtrl', function($scope, $ionicNavBarDelegate, $http) {
 	var isIOS = ionic.Platform.isIOS();
 	$ionicNavBarDelegate.showBackButton(isIOS);
+
+	$scope.especialidades = [];
+	$http.get('http://apmsantos.org.br/indicadorMedico/webservice/especialidades')
+	.success(function(especialidades){
+		$scope.especialidades = especialidades;
+	}).error(function(erro){
+		console.log(erro);
+	})
 })
    
 .controller('especialidadeCtrl', function($scope) {

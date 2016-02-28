2cbb32a6102c5b50cb184cf34c1cf765ece1f085
Gustavo <gustavomartinis@gmail.com>
2016-02-28 16:00:07 -0300

    controller só faz o get das informações na primeira vez que abre. Após isso salva os dados no aplicativo.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,33 +3,52 @@ angular.module('app.controllers', [])
 .controller('indicadorMDicoCtrl', function($scope, $ionicNavBarDelegate, $http) {
 	var isIOS = ionic.Platform.isIOS();
 	$ionicNavBarDelegate.showBackButton(isIOS);
-
+	//window.localStorage.clear();
 	$scope.especialidades = [];
-	$http.get('http://apmsantos.org.br/indicadorMedico/webservice/especialidades')
-	.success(function(especialidades){
+	especialidades = JSON.parse(window.localStorage.getItem( 'especialidades' ));
+	if(especialidades != null){
 		$scope.especialidades = especialidades;
-	}).error(function(erro){
-		console.log(erro);
-	})
+	}else{
+		$http.get('http://apmsantos.org.br/indicadorMedico/webservice/especialidades')
+		.success(function(especialidades){
+			window.localStorage.setItem( 'especialidades', JSON.stringify(especialidades) );
+			$scope.especialidades = especialidades;
+		}).error(function(erro){
+			console.log(erro);
+		})
+	}
 })
    
 .controller('especialidadeCtrl', function($scope, $stateParams, $http) {
-	$scope.nomeEspecialidade = $stateParams.especialidadeNome;
 	$scope.medicos = [];
-	$http.get('http://apmsantos.org.br/indicadorMedico/webservice/medicosEspecialistas/' + $stateParams.especialidadeId)
-	.success(function(medicos){
+	medicos = JSON.parse(window.localStorage.getItem( 'medicos'+ $stateParams.especialidadeId ));
+	if(medicos != null){
 		$scope.medicos = medicos;
-	}).error(function(erro){
-		console.log(erro);
-	})
+	}else{
+		$scope.nomeEspecialidade = $stateParams.especialidadeNome;
+		$http.get('http://apmsantos.org.br/indicadorMedico/webservice/medicosEspecialistas/' + $stateParams.especialidadeId)
+		.success(function(medicos){
+			window.localStorage.setItem( 'medicos'+ $stateParams.especialidadeId, JSON.stringify(medicos) );
+			$scope.medicos = medicos;
+		}).error(function(erro){
+			console.log(erro);
+		})
+	}
 })
    
 .controller('medicoCtrl', function($scope, $stateParams, $http) {
 	$scope.medico = [];
-	$http.get('http://apmsantos.org.br/indicadorMedico/webservice/medico/' + $stateParams.medicoId)
-	.success(function(medico){
+	medico = JSON.parse(window.localStorage.getItem( 'medico'+ $stateParams.medicoId ));
+	if(medico != null){
+		console.log(medico);
 		$scope.medico = medico;
-	}).error(function(erro){
-		console.log(erro);
-	})
+	}else{
+		$http.get('http://apmsantos.org.br/indicadorMedico/webservice/medico/' + $stateParams.medicoId)
+		.success(function(medico){
+			window.localStorage.setItem( 'medico'+ $stateParams.medicoId, JSON.stringify(medico) );
+			$scope.medico = medico;
+		}).error(function(erro){
+			console.log(erro);
+		})
+	}
 })

b69b366f9798b48c80885e489bb1f98d76ed81e4
Gustavo <gustavomartinis@gmail.com>
2016-03-03 12:46:45 -0300

    Colocado loading nas paginas e botão para navegação no médico. Alterado titulo da home.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('app.controllers', [])
   
-.controller('indicadorMedicoCtrl', function($scope, $ionicNavBarDelegate, getEspecialidades, $ionicFilterBar) {
+.controller('indicadorMedicoCtrl', function($scope, $ionicNavBarDelegate, getEspecialidades, $ionicFilterBar, $ionicLoading) {
 	var isIOS = ionic.Platform.isIOS();
 	$ionicNavBarDelegate.showBackButton(isIOS);
 	//window.localStorage.clear();
@@ -25,10 +25,14 @@ angular.module('app.controllers', [])
 	if(especialidades != null){
 		$scope.especialidades = especialidades;
 	}else{
+		$ionicLoading.show({
+	      template: '<ion-spinner icon="android"/>'
+	    });
 		getEspecialidades.buscar()
 		.then(function(dados){
 			window.localStorage.setItem( 'especialidades', JSON.stringify(dados.especialidades) );
 			$scope.especialidades = dados.especialidades;
+			$ionicLoading.hide();
 		})
 	};
 	$scope.doRefresh = function() {
@@ -43,17 +47,21 @@ angular.module('app.controllers', [])
 	};
 })
    
-.controller('especialidadeCtrl', function($scope, $stateParams, getMedicos) {
+.controller('especialidadeCtrl', function($scope, $stateParams, getMedicos, $ionicLoading) {
 	var id = $stateParams.especialidadeId;
 	medicos = JSON.parse(window.localStorage.getItem( 'medicos' + id ));
 	if(medicos != null){
 		$scope.medicos = medicos;
 	}else{
+		$ionicLoading.show({
+	      template: '<ion-spinner icon="android"/>'
+	    });
 		$scope.nomeEspecialidade = $stateParams.especialidadeNome;
 		getMedicos.buscar(id)
 		.then(function(dados){
 			window.localStorage.setItem( 'medicos' + id, JSON.stringify(dados.medicos) );
 			$scope.medicos = dados.medicos;
+			$ionicLoading.hide();
 		})
 	};
 	$scope.doRefresh = function() {
@@ -67,17 +75,21 @@ angular.module('app.controllers', [])
 	};
 })
    
-.controller('medicoCtrl', function($scope, $stateParams, getDadosDoMedico) {
+.controller('medicoCtrl', function($scope, $stateParams, getDadosDoMedico, $ionicLoading) {
 	$scope.medico = [];
 	var id = $stateParams.medicoId;
 	medico = JSON.parse(window.localStorage.getItem( 'medico'+ id));
 	if(medico != null){
 		$scope.medico = medico;
 	}else{
+		$ionicLoading.show({
+	      template: '<ion-spinner icon="android"/>'
+	    });
 		getDadosDoMedico.buscar(id)
 		.then(function(dados){
 			window.localStorage.setItem( 'medico'+ id, JSON.stringify(dados.medico) );
 			$scope.medico = dados.medico;
+			$ionicLoading.hide();
 		})
 	}
 	$scope.doRefresh = function() {
@@ -89,4 +101,26 @@ angular.module('app.controllers', [])
 	       $scope.$broadcast('scroll.refreshComplete');
 	     });
 	};
+
+	$scope.showMap = function(){
+		$ionicLoading.show({
+	      template: '<ion-spinner icon="android"/>'
+	    });
+		launchnavigator.navigate(
+			medico.endereco,
+			'',
+			function(){
+		    	//alert("Plugin success");
+		    	$scope.hide = function(){
+			    $ionicLoading.hide();
+			  };
+			},
+			function(error){
+		    	alert("Plugin error: "+ error);
+			},
+			{
+	    		preferGoogleMaps: true
+			}
+		);
+	}
 })
